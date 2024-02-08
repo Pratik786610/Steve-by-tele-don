d6dba8ee69e027183966bbb874e749cfaa9e5524
Pratik786610 <152353594+Pratik786610@users.noreply.github.com>
2024-02-08 10:02:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -95,7 +95,7 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
 SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 300))
 DOWNLOAD_TEXT_URL = "https://t.me/+pVPv69822bhmMmZl"
-CAPTION_BUTTON_URL = "https://t.me/haiabb"
+CAPTION_BUTTON_URL = "https://t.me/updatesishe"
 WEBHOOK = bool(environ.get("WEBHOOK", True))
 BUTTON_LOCK = environ.get("BUTTON_LOCK", "True")
 RemoveBG_API = environ.get("RemoveBG_API", "eQ9W36MSu3pmQJLVPZHmF47F")
